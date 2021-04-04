21884b7289b11ef1ed741ba8c5d872e642953aea
Agustin Carrasco <asermax@gmail.com>
2021-04-04 20:51:57 -0300

    fix(cfp): remove length restriction for talk description

--- components/pages/cfp/Step4.jsx
+++ components/pages/cfp/Step4.jsx
@@ -25,10 +25,7 @@ export const Step4 = () => {
           Ahora sí, contanos en todo el detalle que quieras de qué se trata tu propuesta.
         </Step.FieldDescription>
         <TalkDescriptionField>
-          <Textarea
-            {...register('talkShortDescription', { required: true, maxLength: 200 })}
-            autoFocus
-          />
+          <Textarea {...register('talkShortDescription', { required: true })} autoFocus />
         </TalkDescriptionField>
       </TalkDescriptionFieldContainer>
     </>
