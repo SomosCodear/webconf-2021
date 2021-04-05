1c2795bac3d3ceead09e3026228879d785d44ca8
homer0 <me@homer0.dev>
2021-04-05 17:22:52 -0300

    fix(api/cfp): manually configure AWS SDK

--- pages/api/cfp.js
+++ pages/api/cfp.js
@@ -3,8 +3,9 @@ import AWSSDK from 'aws-sdk';
 import nodemailer from 'nodemailer';
 
 const CONFIG = {
-  address: process.env.CFP_NOTIFICATIONS_EMAIL || 'contenido@codear.org',
-  notificationsSubject: 'Alguien mando una propuesta de charla!',
+  address: process.env.WEBCONF_2021_CFP_NOTIFICATIONS_EMAIL || 'contenido@webconf.tech',
+  notificationsSubject:
+    process.env.WEBCONF_2021_CFP_NOTIFICATIONS_SUBJECT || 'Alguien mando una propuesta de charla!',
 };
 
 const ERRORS = {
@@ -50,66 +51,66 @@ const ERRORS = {
 };
 
 const schema = joi.object({
-  checkGuidelines: joi.boolean().required()
-    .messages({
-      'boolean.base': ERRORS.checkGuidelines.required,
-      'any.required': ERRORS.checkGuidelines.required,
-    }),
-  talkTitle: joi.string().min(5).required()
-    .messages({
-      'string.min': ERRORS.talkTitle.minLength,
-      'string.base': ERRORS.talkTitle.required,
-      'any.required': ERRORS.talkTitle.required,
-    }),
-  talkStandardLength: joi.boolean().required()
-    .messages({
-      'boolean.base': ERRORS.talkStandardLength.required,
-      'any.required': ERRORS.talkStandardLength.required,
-    }),
-  talkTweet: joi.string().min(10).max(200).required()
-    .messages({
-      'string.min': ERRORS.talkTweet.minLength,
-      'string.max': ERRORS.talkTweet.maxLength,
-      'string.base': ERRORS.talkTweet.required,
-      'any.required': ERRORS.talkTweet.required,
-    }),
-  talkHashtags: joi.string().pattern(/(?:#[a-zA-Z0-9_\-$]+(?:\s|$))+$/).required()
+  checkGuidelines: joi.boolean().required().messages({
+    'boolean.base': ERRORS.checkGuidelines.required,
+    'any.required': ERRORS.checkGuidelines.required,
+  }),
+  talkTitle: joi.string().min(5).required().messages({
+    'string.min': ERRORS.talkTitle.minLength,
+    'string.base': ERRORS.talkTitle.required,
+    'any.required': ERRORS.talkTitle.required,
+  }),
+  talkStandardLength: joi.boolean().required().messages({
+    'boolean.base': ERRORS.talkStandardLength.required,
+    'any.required': ERRORS.talkStandardLength.required,
+  }),
+  talkTweet: joi.string().min(10).max(200).required().messages({
+    'string.min': ERRORS.talkTweet.minLength,
+    'string.max': ERRORS.talkTweet.maxLength,
+    'string.base': ERRORS.talkTweet.required,
+    'any.required': ERRORS.talkTweet.required,
+  }),
+  talkHashtags: joi
+    .string()
+    .pattern(/(?:#[a-zA-Z0-9_\-$]+(?:\s|$))+$/)
+    .required()
     .messages({
       'string.pattern.base': ERRORS.talkHashtags.pattern,
       'string.base': ERRORS.talkHashtags.required,
       'any.required': ERRORS.talkHashtags.required,
     }),
-  talkSummary: joi.string().min(10).required()
-    .messages({
-      'string.min': ERRORS.talkSummary.minLength,
-      'string.base': ERRORS.talkSummary.required,
-      'any.required': ERRORS.talkSummary.required,
-    }),
-  speakerName: joi.string().min(5).required()
-    .messages({
-      'string.min': ERRORS.speakerName.minLength,
-      'string.base': ERRORS.speakerName.required,
-      'any.required': ERRORS.speakerName.required,
-    }),
-  speakerIsAdult: joi.boolean().required()
-    .messages({
-      'boolean.base': ERRORS.speakerIsAdult.required,
-      'any.required': ERRORS.speakerIsAdult.required,
-    }),
-  speakerCity: joi.string().min(10).required()
-    .messages({
-      'string.min': ERRORS.speakerCity.minLength,
-      'string.base': ERRORS.speakerCity.required,
-      'any.required': ERRORS.speakerCity.required,
-    }),
-  speakerEmail: joi.string().min(5).email().required()
-    .messages({
-      'string.email': ERRORS.speakerEmail.invalidEmail,
-      'string.min': ERRORS.speakerEmail.minLength,
-      'string.base': ERRORS.speakerEmail.required,
-      'any.required': ERRORS.speakerEmail.required,
-    }),
+  talkSummary: joi.string().min(10).required().messages({
+    'string.min': ERRORS.talkSummary.minLength,
+    'string.base': ERRORS.talkSummary.required,
+    'any.required': ERRORS.talkSummary.required,
+  }),
+  speakerName: joi.string().min(5).required().messages({
+    'string.min': ERRORS.speakerName.minLength,
+    'string.base': ERRORS.speakerName.required,
+    'any.required': ERRORS.speakerName.required,
+  }),
+  speakerIsAdult: joi.boolean().required().messages({
+    'boolean.base': ERRORS.speakerIsAdult.required,
+    'any.required': ERRORS.speakerIsAdult.required,
+  }),
+  speakerCity: joi.string().min(10).required().messages({
+    'string.min': ERRORS.speakerCity.minLength,
+    'string.base': ERRORS.speakerCity.required,
+    'any.required': ERRORS.speakerCity.required,
+  }),
+  speakerEmail: joi.string().min(5).email().required().messages({
+    'string.email': ERRORS.speakerEmail.invalidEmail,
+    'string.min': ERRORS.speakerEmail.minLength,
+    'string.base': ERRORS.speakerEmail.required,
+    'any.required': ERRORS.speakerEmail.required,
+  }),
   extra: joi.string(),
+});
+
+AWSSDK.config.update({
+  accessKeyId: process.env.WEBCONF_2021_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.WEBCONF_2021_AWS_SECRET_ACCESS_KEY,
+  region: process.env.WEBCONF_2021_AWS_REGION || 'us-east-1',
 });
 
 const mailer = nodemailer.createTransport({
@@ -124,19 +125,14 @@ const keyToLabel = (key) => {
 export default async (req, res) => {
   const data = schema.validate(req.body, { stripUnknown: true });
   if (data.error) {
-    return res
-      .status(400)
-      .json({
-        error: data.error.message,
-      });
+    return res.status(400).json({
+      error: data.error.message,
+    });
   }
 
   const report = Object.entries(data.value)
     .reduce(
-      (acc, [key, value]) => [
-        ...acc,
-        `<strong>${keyToLabel(key)}:</strong><br />${value}`,
-      ],
+      (acc, [key, value]) => [...acc, `<strong>${keyToLabel(key)}:</strong><br />${value}`],
       [],
     )
     .join('<br /><br />')
@@ -152,10 +148,8 @@ export default async (req, res) => {
 
     return res.status(200).json({ success: true });
   } catch (error) {
-    return res
-      .status(500)
-      .json({
-        error: error.message,
-      });
+    return res.status(500).json({
+      error: error.message,
+    });
   }
 };
