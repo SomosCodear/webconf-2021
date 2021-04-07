3f5e407f1b65b6906758a4f65a5e8341e815150e
homer0 <me@homer0.dev>
2021-04-06 23:48:11 -0300

    feat(api/cfp): add email confirmation

--- pages/api/cfp.js
+++ pages/api/cfp.js
@@ -2,11 +2,53 @@ import * as joi from 'joi';
 import AWSSDK from 'aws-sdk';
 import nodemailer from 'nodemailer';
 
-const CONFIG = {
-  address: process.env.WEBCONF_2021_CFP_NOTIFICATIONS_EMAIL || 'contenido@webconf.tech',
-  notificationsSubject:
-    process.env.WEBCONF_2021_CFP_NOTIFICATIONS_SUBJECT || 'Alguien mando una propuesta de charla!',
+const envOr = (varName, fallback = '') => process.env[`WEBCONF_2021_CFP_${varName}`] || fallback;
+
+const keyToLabel = (key) => {
+  const label = key.replace(/([a-z])([A-Z])/g, '$1 $2');
+  return `${label[0].toUpperCase()}${label.substr(1).toLowerCase()}`;
 };
+
+const dictionaryToReport = (dict) =>
+  Object.entries(dict)
+    .reduce(
+      (acc, [key, value]) => [...acc, `<strong>${keyToLabel(key)}:</strong><br />${value}`],
+      [],
+    )
+    .join('<br /><br />')
+    .replace(/(?:<br \/>)+$/, '');
+
+const DEFAULT_ADDRESS = 'contenido@webconf.tech';
+const CONFIG = {
+  notification: {
+    address: envOr('NOTIFICATIONS_EMAIL', DEFAULT_ADDRESS),
+    subject: envOr('NOTIFICATIONS_SUBJECT', '¡Nueva propuesta de charla!'),
+  },
+  confirmation: {
+    address: envOr('CONFIRMATION_EMAIL', DEFAULT_ADDRESS),
+    subject: envOr('CONFIRMATION_SUBJECT', 'WebConf 2021 - ¡Gracias por sumarte!'),
+  },
+};
+
+/* eslint-disable max-len */
+const CONFIRMATION_TEMPLATE = [
+  '<p>¡Hola!</p>',
+  '<p>Queremos agradecerte por habernos mandado tu propuesta para la WebConf 2020; nos pone súper-contentos ver la confianza y el interés que mostrás en esta nueva edición de la conferencia.</p>',
+  '<p>Durante las próximas semanas vamos a estar analizando tu charla para entender en profundidad la temática que propusiste. Te contactaremos nuevamente acercándote novedades, dudas o feedback sobre tu postulación.</p>',
+  '<p>Te pedimos que tengas paciencia y estés pendiente de nuestros mails. Dependiendo del volumen de propuestas puede que tardemos en contactarnos, pero nuestra intención es la de tener una buena cadencia para avanzar con el proceso lo más rápido posible.</p>',
+  '<p>De nuevo, ¡muchas gracias por animarte y seguimos en contacto!</p>',
+  '<p>--</p>',
+  '<p>',
+  '<a href="https://codear.org"><img src="https://i.imgur.com/ZlaT5Go.png" alt="Logo de CoDeAr" /></a>',
+  '<br /><strong>Equipo de contenido de WebConf</strong>',
+  '</p>',
+  '<p>',
+  '<strong><em>Nuestras redes sociales</em></strong><br />',
+  '<strong>Instagram:</strong> https://instagram.com/somoscodear<br />',
+  '<strong>Twitter:</strong> https://twitter.com/somoscodear<br />',
+  '</p>',
+].join('\n');
+/* eslint-enable max-len */
 
 const ERRORS = {
   checkGuidelines: {
@@ -117,11 +159,6 @@ const mailer = nodemailer.createTransport({
   SES: new AWSSDK.SES(),
 });
 
-const keyToLabel = (key) => {
-  const label = key.replace(/([a-z])([A-Z])/g, '$1 $2');
-  return `${label[0].toUpperCase()}${label.substr(1).toLowerCase()}`;
-};
-
 export default async (req, res) => {
   const data = schema.validate(req.body, { stripUnknown: true });
   if (data.error) {
@@ -130,21 +167,21 @@ export default async (req, res) => {
     });
   }
 
-  const report = Object.entries(data.value)
-    .reduce(
-      (acc, [key, value]) => [...acc, `<strong>${keyToLabel(key)}:</strong><br />${value}`],
-      [],
-    )
-    .join('<br /><br />')
-    .replace(/(?:<br \/>)+$/, '');
-
   try {
-    await mailer.sendMail({
-      from: CONFIG.address,
-      to: CONFIG.address,
-      subject: CONFIG.notificationsSubject,
-      html: report,
-    });
+    await Promise.all([
+      mailer.sendMail({
+        from: CONFIG.notification.address,
+        to: CONFIG.notification.address,
+        subject: CONFIG.notification.subject,
+        html: dictionaryToReport(data.value),
+      }),
+      mailer.sendMail({
+        from: CONFIG.confirmation.address,
+        to: data.value.speakerEmail,
+        subject: CONFIG.confirmation.subject,
+        html: CONFIRMATION_TEMPLATE,
+      }),
+    ]);
 
     return res.status(200).json({ success: true });
   } catch (error) {
