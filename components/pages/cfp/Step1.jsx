628db1642f7f689e6113388212ef93e4c8450a85
Agustin Carrasco <asermax@gmail.com>
2021-04-09 00:13:08 -0300

    fix(cfp): make guidelines check label bold

--- components/pages/cfp/Step1.jsx
+++ components/pages/cfp/Step1.jsx
@@ -5,7 +5,7 @@ import { cfpFieldValidations } from '~/services/cfp';
 import { Checkbox } from '~/components/common';
 import { Step } from './Step';
 
-const TermsDescription = styled.p`
+const GuidelinesDescription = styled.p`
   margin-top: 1rem;
   font-size: 1.25rem;
   font-weight: 400;
@@ -14,6 +14,10 @@ const TermsDescription = styled.p`
     margin-top: 2rem;
     font-size: 1.75rem;
   }
+`;
+
+const GuidelinesCheckbox = styled(Checkbox)`
+  font-weight: 500;
 `;
 
 export const Step1 = () => {
@@ -25,23 +29,23 @@ export const Step1 = () => {
   return (
     <>
       <Step.Title>Antes de continuar...</Step.Title>
-      <TermsDescription>
+      <GuidelinesDescription>
         Te pedimos que te asegures de haber leído todos los lineamientos de selección expuestos en{' '}
         <a href="https://codear.org" target="_blank" rel="noreferrer">
           nuestra web
         </a>
         .
-      </TermsDescription>
+      </GuidelinesDescription>
       <Step.FieldContainer>
         <Step.Field>
-          <Checkbox
+          <GuidelinesCheckbox
             id="terms-checkbox"
             {...register('checkGuidelines')}
             hasError={errors.checkGuidelines != null}
             autoFocus
           >
             Declaro que leí los lineamientos de selección.
-          </Checkbox>
+          </GuidelinesCheckbox>
           {errors.checkGuidelines ? (
             <Step.FieldError>{errors.checkGuidelines.message}</Step.FieldError>
           ) : null}
