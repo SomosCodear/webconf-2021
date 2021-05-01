7e742ede778cdb97a52296ba299423ada7935877
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-05-01 15:55:29 -0300

    chore(docs): added 2021 brief pdf

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/brief2021',
+        destination: '/brief-2021.pdf',
+      },
+    ];
+  },
 };
