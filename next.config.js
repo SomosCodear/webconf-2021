05bbf8edf7d7ae7ed8d5265361c82c4a6a5275d8
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-05-02 10:25:49 -0300

    chore(config): set redirects as temporary

--- next.config.js
+++ next.config.js
@@ -20,10 +20,12 @@ module.exports = {
         source: '/videos/2019',
         destination:
           'https://www.youtube.com/watch?v=h6sbrtMnMzM&list=PL7JGDc7wfszTq9oLtahn3vvohLOsC7JT4',
+        permanent: false,
       },
       {
         source: '/videos/2020',
         destination: 'https://www.youtube.com/hashtag/encuentroswebconf',
+        permanent: false,
       },
     ];
   },
