cf8023df7182faa08720b1312e771760156bd838
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-05-02 10:20:05 -0300

    chore(config): added redirect links for webconf assets

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ module.exports = {
         source: '/brief2021',
         destination: '/brief-2021.pdf',
       },
+      {
+        source: '/briefs/2021',
+        destination: '/brief-2021.pdf',
+      },
+      {
+        source: '/videos/2019',
+        destination:
+          'https://www.youtube.com/watch?v=h6sbrtMnMzM&list=PL7JGDc7wfszTq9oLtahn3vvohLOsC7JT4',
+      },
+      {
+        source: '/videos/2020',
+        destination: 'https://www.youtube.com/hashtag/encuentroswebconf',
+      },
     ];
   },
 };
