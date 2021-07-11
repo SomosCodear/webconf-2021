4e1b4c8aaeda3c4299e0a9f1824b673da5f11cfe
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-07-11 16:00:41 -0300

    feat(cfp): workshop mode

--- components/pages/cfp/CFP.jsx
+++ components/pages/cfp/CFP.jsx
@@ -1,4 +1,5 @@
 import * as R from 'ramda';
+import propTypes from 'prop-types';
 import { useState, useCallback, useEffect } from 'react';
 import { useMutation } from 'react-query';
 import styled from 'styled-components';
@@ -155,7 +156,16 @@ const ProgressArrow = styled.div`
   }
 `;
 
-export const CFP = () => {
+const messages = {
+  talks: {
+    subject: 'charla',
+  },
+  workshops: {
+    subject: 'taller',
+  },
+};
+
+export const CFP = ({ type = 'talks' }) => {
   const [currentStep, setCurrentStep] = useState(0);
   const [stepsData, setStepsData] = useState(INITAL_STEPS_DATA);
   const CurrentStepComponent = STEPS[currentStep];
@@ -245,7 +255,7 @@ export const CFP = () => {
       {currentStep < STEPS.length ? (
         <>
           <Progress>
-            <h1>¡Proponé tu charla para WebConf 2021!</h1>
+            <h1>¡Proponé tu {messages[type].subject} para WebConf 2021!</h1>
             <ProgressBar>
               {STEPS.map((_, index) => (
                 <ProgressStep key={index /* eslint-disable-line react/no-array-index-key */} />
@@ -263,7 +273,7 @@ export const CFP = () => {
             defaultValues={stepsData[currentStep]}
             validationSchema={CurrentStepComponent.validationSchema}
           >
-            <CurrentStepComponent />
+            <CurrentStepComponent type={type} />
           </Step>
         </>
       ) : null}
@@ -271,4 +281,12 @@ export const CFP = () => {
       {isSuccess ? <Success onReset={reset} /> : null}
     </Container>
   );
+};
+
+CFP.propTypes = {
+  type: propTypes.oneOf(['talks', 'workshops']),
+};
+
+CFP.defaultProps = {
+  type: 'talks',
 };
