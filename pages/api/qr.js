1e90d6450fb0cec8b637dbcf7f5b5b7d4468d5e9
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-08-18 04:44:10 -0300

    fix(qr-api): updated qr data

--- pages/api/qr.js
+++ pages/api/qr.js
@@ -3,7 +3,10 @@ import fetch from 'node-fetch';
 export default async (req, res) => {
   let url = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=WEBCONF2021&margin=10';
   if (req.query.rombianUserId) {
-    url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${req.query.rombianUserId}&margin=10`;
+    const data = encodeURIComponent(
+      `https://webconf.tech/en-vivo/?rombianUserId=${req.query.rombianUserId}`,
+    );
+    url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}&margin=10`;
   }
 
   const qrResponse = await fetch(url);
